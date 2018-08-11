bcfbc125c177363fc7398fb3aacdcb0888610c26
clip <binderclip2014@gmail.com>
2018-08-11 18:56:40 +0800

    recreate

--- apps/hello_rectangle/lib/main.dart
+++ apps/hello_rectangle/lib/main.dart
@@ -1,21 +1,16 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello Rectangle',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Rectangle'),
-        ),
-        body: HelloRectangle(),
-      ),
-    ),
+        debugShowCheckedModeBanner: false,
+        title: 'Hello Rectangle',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Hello Rectangle'),
+          ),
+          body: HelloRectangle(),
+        )),
   );
 }
 
