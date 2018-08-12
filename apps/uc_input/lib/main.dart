c0bd70296d50668ea784ca52b01d63b4cec6287a
clip <binderclip2014@gmail.com>
2018-08-12 16:13:09 +0800

    udacity course, input

---
+++ apps/uc_input/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'category_route.dart';
+
+void main() {
+  runApp(UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        primaryColor: Colors.grey[400],
+        textSelectionHandleColor: Colors.green[500],
+      ),
+      home: CategoryRoute(),
+    );
+  }
+}
