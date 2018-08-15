149ece3472e27f9a0156a6b1050737c518a1c40c
clip <binderclip2014@gmail.com>
2018-08-15 20:01:29 +0800

    add labo

---
+++ apps/labo/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:fluttery/framing.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RandomColorBlock();
+  }
+}
